ec981057c10c222574f95bfc58baecdceea0373e
AlanASC4 <alan.torres0229@gmail.com>
2023-04-03 21:13:49 -0400

    db

--- dbcontroller.php
+++ dbcontroller.php
@@ -3,7 +3,7 @@ class DBController {
 	private $host = "localhost";
 	private $user = "root";
 	private $password = "";
-	private $database = "blog_samples";
+	private $database = "myDB";
 	private $conn;
 	
 	function __construct() {
